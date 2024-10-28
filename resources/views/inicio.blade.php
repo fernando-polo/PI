d0b1d6407d030ffe40be181ef4a78fa1fad3ec55
fernando-polo <fernandogmzmdo@gmail.com>
2024-10-27 22:17:48 -0600

    Landing Page terminada.
    Se terminó de crear la landing page.

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -8,6 +8,7 @@
 @section('contenido')
 
 {{-- Hero Section --}}
+<section style="min-height: 65vh;">
     <br>
     <br>
         <div class="container">
@@ -33,9 +34,10 @@
         </div> 
     <br>
     <br>
-
+</section>
 {{-- Nuestros Estudios --}}
-    {{-- <br>
+<section style="min-height: 75vh;">
+    <br>
     <br>
     <div class="container text-center">
         <br>
@@ -45,63 +47,242 @@
         <hr class="hr-custom text-center">
         <p style="font-size: 20px; font-weight: bold;">Nuestra <span style="color: #484FFA">clínica médica</span> brinda una amplia variedad de <span style="color: #484FFA">análisis para tu salud,</span> te ayudamos con el proceso</p>
 
-        Carrusel
-        <div id="carouselExample" class="carousel slide">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
+        {{-- Cards --}}
+        <div class="container ">
+          <div class="row gap-4 justify-content-center">
+            <div class="card align-items-center justify-content-center">
+              <div class="row">
+                  <img src="{{ asset('images/NE_Mastografia.jpg') }}" class="card-img-top" alt="Card Mastografia" id="Card_Img_NE_Mastografia">
               </div>
-              <div class="carousel-item">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
+              <br>
+              <div class="row">
+                  <a href="#" class="btn btn-sm btn-outline-secondary">Mastografía</a>
               </div>
-              <div class="carousel-item">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                  </div>
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
           </div>
-
-
-
+          
+          <div class="card align-items-center justify-content-center">
+              <div class="row">
+                  <img src="{{ asset('images/NE_Densitometria.jpg') }}" class="card-img-top" alt="Card Densitometria" id="Card_Img_NE_Densitometria">
+              </div>
+              <br>
+              <div class="row">
+                  <a href="#" class="btn btn-sm btn-outline-secondary">Densitometría</a>
+              </div>
+          </div>
+          
+          <div class="card align-items-center justify-content-center">
+              <div class="row">
+                  <img src="{{ asset('images/NE_Papanicolau.jpg') }}" class="imagen_card card-img-top" alt="Card Papanicolau" id="Card_Img_NE_Papanicolau">
+              </div>
+              <br>
+              <div class="row">
+                  <a href="#" class="btn btn-sm btn-outline-secondary">Papanicolau</a>
+              </div>
+          </div>
+          
+          <div class="card align-items-center justify-content-center">
+              <div class="row">
+                  <img src="{{ asset('images/NE_Ultrasonido.jpg') }}" class="card-img-top" alt="Card Ultrasonido" id="Card_Img_NE_Ultrasonido">
+              </div>
+              <br>
+              <div class="row">
+                  <a href="#" class="btn btn-sm btn-outline-secondary">Ultrasonido</a>
+              </div>
+          </div>
+          
+          <div class="card align-items-center justify-content-center">
+              <div class="row">
+                  <img src="{{ asset('images/NE_VPH.jpg') }}" class="card-img-top" alt="Card VPH" id="Card_Img_NE_VPH">
+              </div>
+              <br>
+              <div class="row">
+                  <a href="#" class="btn btn-sm btn-outline-secondary">VPH</a>
+              </div>
+          </div>
+          
+            
+            
+          </div>
+        </div>
     </div>
     <br>
-    <br> --}}
+    <br>
+</section>
+
+{{-- Médicos de primera --}}
+<section style="min-height: 75vh;">
+  <br>
+  <br>
+  <div class="container text-center">
+    <h5><span style="color: #484FFA; font-size: 32px; font-weight: bold;"><strong>Seguridad y bienestar</strong></span></h5>
+    <h5 style="font-size: 68px; font-weight: bold;"><strong><span style="color: #6285C4;"> Médicos</span> de primera</strong></h5>
+    <hr class="hr-custom text-center">
+    <p style="font-size: 20px; font-weight: bold;">Nuestros  <span style="color: #484FFA">médicos</span> con más de <span style="color: #484FFA">20 años de experiencia </span> para el servicio tuyo y de tu familia.</p>
+
+    <br>
+
+    <div class="row">
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Dr. Myrna Maldonado</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">Especialidad</span><p>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">No. de Cédula</span></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Dr. Myrna Maldonado</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">Especialidad</span><p>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">No. de Cédula</span></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Dr. Myrna Maldonado</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">Especialidad</span><p>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">No. de Cédula</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Dr. Myrna Maldonado</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">Especialidad</span><p>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">No. de Cédula</span></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Dr. Myrna Maldonado</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">Especialidad</span><p>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">No. de Cédula</span></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Dr. Myrna Maldonado</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">Especialidad</span><p>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">No. de Cédula</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+</section>
+
+{{-- Tu Testimonio importa --}}
+<section style="min-height: 75vh;">
+  <br>
+  <br>
+  <div class="container text-center">
+    <h5><span style="color: #484FFA; font-size: 32px; font-weight: bold;"><strong>Sinceridad ante todo</strong></span></h5>
+    <h5 style="font-size: 68px; font-weight: bold;"><strong>Tu<span style="color: #6285C4;"> Testimonio</span> importa</strong></h5>
+    <hr class="hr-custom text-center">
+    <p style="font-size: 20px; font-weight: bold;"><span style="color: #484FFA">Cuidamos de tu familia y de ti, </span>los siguientes testimnoios lo demuestran</p>
+
+    <br>
+
+    <div class="row">
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Nombre - Usuario</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">'@ Usuario'</span><p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Nombre - Usuario</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">'@ Usuario'</span><p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Nombre - Usuario</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">'@ Usuario'</span><p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Nombre - Usuario</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">'@ Usuario'</span><p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Nombre - Usuario</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">'@ Usuario'</span><p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4 mb-3 mb-sm-0">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title"><span style="color: #484FFA; font-size: 20px; font-weight: bold;">Nombre - Usuario</span></h5>
+            <p class="card-text"><span style="color: #6285C4; font-size: 16px; font-weight: bold;">'@ Usuario'</span><p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+</section>
+
+
+{{-- Sobre nosotros --}}
+<section style="min-height: 75vh;">
+  <br>
+  <br>
+  <div class="container text-center">
+    <h5><span style="color: #484FFA; font-size: 32px; font-weight: bold;"><strong>Nuestra historia como institución</strong></span></h5>
+    <h5 style="font-size: 68px; font-weight: bold;"><strong>Sobre <span style="color: #6285C4;">Nosotros</span></strong></h5>
+    <hr class="hr-custom text-center">
+    <div class="row align-items-center">
+      <div class="col">
+        <br>
+        <p class="text-start"> <span style="color: #484FFA; font-size: 16px; font-weight: bold;">La Clínica MedCLINIC</span> es un centro de atención integral que ofrece una amplia gama de servicios médicos en cuanto a la detección de cáncer a termpana edad. Con un equipo de profesionales altamente capacitados, modernas instalaciones y tecnología de vanguardia granatizamos a mejor experiencia y bienestar de sus pacientes. La Clínica MedCLINIC se compromete a proporcionar un servicio humano y accesible, brindando seguridad y confianza en cada consulta.</p>
+      </div>
+      <div class="col">
+        <img src="{{ asset('images/Logo1.jpeg') }}" alt="Logo - Clínica Médica" class="img-fluid text-start" style="max-width: 100%; height: auto; max-height: 400px;">
+      </div>
+    </div>
+  </div>
+  <br>
+  <br>
+</section>
 @endsection
 
 
