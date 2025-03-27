0957cf5493d2bf88acf0aa577c6a8ddc8f249ea3
MichelleQM <148285778+MichelleQM@users.noreply.github.com>
2025-03-27 15:02:07 -0600

    mich

--- resources/views/vistasMedicos.blade.php
+++ resources/views/vistasMedicos.blade.php
@@ -5,21 +5,21 @@
 @section('btn1')
     <a href="{{route('rutavistasMedicos')}}" id="btnIconoCalendario" class="btn btn-outline-primary">
         <i class="bi bi-calendar"></i>        
-        Calendario
+        Usuarios 
     </a>
 @endsection
 
 @section('btn2')
     <a href="{{route('rutavistasMedicosPC')}}" id="btnIconoCitasProximas" class="btn btn-outline-primary">
         <i class="bi bi-calendar-plus"></i>
-        Próximas citas
+        Medicos
     </a>
 @endsection
 
 @section('btn3')
     <a href="{{route('rutavistasMedicosCR')}}" id="btnIconoCitasRealizadas" class="btn btn-outline-primary">
         <i class="bi bi-calendar-check"></i>        
-        Citas realizadas
+        Estudios
     </a>
 @endsection
 
